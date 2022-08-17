023bfc31d1930f21dcf080c31c85c7d3db1fac7f
Nddakhil <109868614+Nddakhil@users.noreply.github.com>
2022-08-17 20:56:54 +0300

    commit

--- cw5/cw5/ContentView.swift
+++ cw5/cw5/ContentView.swift
@@ -11,53 +11,15 @@ struct ContentView: View {
     @State var counter: [Int] = [0, 0, 0]
     var body: some View {
         VStack{
-            HStack{
-                Text("أستغفر الله العظيم").font(.title)
-                Spacer()
-                Text("\(counter[0])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[0] = counter[0] + 1
-                    }
-            }.padding()
-            HStack{
-                Spacer()
-                Text("الحمدلله").font(.title)
-                Spacer()
-                Text("\(counter[1])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[1] = counter[1] + 1
-                    }
-            }.padding()
-            HStack{
-                Text("سبحان الله وبحمده").font(.title)
-                Spacer()
-                Text("\(counter[2])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[2] = counter[2] + 1
-                    }
-            }.padding()
-        }
+        
+            RowView(myCounter: $counter[0], title: "استغفر الله")
+            RowView(myCounter: $counter[1], title: " الحمدالله")
+            RowView(myCounter: $counter[2], title: "سبحان الله")
+            
+        
     }
 }
-
+}
 
 // يجب عمل extract
 
@@ -66,3 +28,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct RowView: View {
+    @Binding var myCounter: Int
+    @State var title: String
+    var body: some View {
+        HStack{
+            Text("أستغفر الله العظيم").font(.title)
+            Spacer()
+            Text("\(myCounter)")
+                .font(.largeTitle)
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.green)
+                .clipShape(Circle())
+                .padding()
+                .onTapGesture {
+                   myCounter = myCounter + 1
+                }
+        }.padding()
+    }
+}
+
+    
+
